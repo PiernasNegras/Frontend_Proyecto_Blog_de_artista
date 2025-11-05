bef455fa724d679b245b572a640a1a6dda86b3f4
PiernasNegras <martinezsnahu@gmail.com>
2025-11-05 02:59:17 -0300

    Actualizo el archivo api.js para que se conecte mediante variables de entorno al backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // Configuro la instancia de Axios para conectar con el backend.
 // Esta instancia se reutiliza en todos los servicios específicos.
 const api = axios.create({
-    baseURL: 'https://backendproyectoblogdeartista-production.up.railway.app',
+    baseURL: process.env.VUE_APP_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
